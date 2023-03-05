5186b405cc948371184e4f4c422fc126b8bf7161
BerryHeyy <berkaykaplanbk@hotmail.com>
2023-03-05 16:07:43 +0100

    Introduced shaders

---
+++ assets/shaders/compile.sh
@@ -0,0 +1,2 @@
+glslc -fshader-stage=frag builtin.object_shader.frag.glsl -o builtin.object_shader.frag.spv
+glslc -fshader-stage=vert builtin.object_shader.vert.glsl -o builtin.object_shader.vert.spv
