e7d511c860dcb5372a71d998e0289b79506a1eaf
BerryHeyy <berkaykaplanbk@hotmail.com>
2023-03-11 14:03:15 +0100

    Switch to blib_darray

--- engine/src/core/event.c
+++ engine/src/core/event.c
@@ -6,15 +6,15 @@
 typedef struct lise_event_entry
 {
 	uint16_t event_code;
-	lise_darray listeners; // lise_on_event_cb
+	blib_darray listeners; // lise_on_event_cb
 } lise_event_entry;
 
-static lise_darray registered_events; // lise_event_entry
+static blib_darray registered_events; // lise_event_entry
 static bool INITIALIZED = false;
 
 bool lise_event_init()
 {
-	registered_events = lise_darray_create(lise_event_entry);
+	registered_events = blib_darray_create(lise_event_entry);
 
 	for (int i = LISE_EVENT_ON_WINDOW_CLOSE; i < LISE_EVENT_MAX_ENUM; i++)
 	{
@@ -31,12 +31,12 @@ void lise_event_shutdown()
 	// Free listeners darrays
 	for (int entry_index = 0; entry_index < registered_events.size; entry_index++)
 	{
-		lise_event_entry* entry = lise_darray_get(&registered_events, entry_index);
+		lise_event_entry* entry = blib_darray_get(&registered_events, entry_index);
 
-		lise_darray_free(&entry->listeners);
+		blib_darray_free(&entry->listeners);
 	}
 
-	lise_darray_free(&registered_events);
+	blib_darray_free(&registered_events);
 
 	LINFO("Successfully shut down the event system.");
 }
@@ -45,22 +45,22 @@ void lise_event_register(uint16_t event_code)
 {
 	lise_event_entry entry = {};
 	entry.event_code = event_code;
-	entry.listeners = lise_darray_create(lise_on_event_cb);
+	entry.listeners = blib_darray_create(lise_on_event_cb);
 
-	lise_darray_push_back(&registered_events, &entry);
+	blib_darray_push_back(&registered_events, &entry);
 }
 
 void lise_event_fire(uint16_t event_code, lise_event_context ctx)
 {
 	for (int entry_index = 0; entry_index < registered_events.size; entry_index++)
 	{
-		lise_event_entry* entry = lise_darray_get(&registered_events, entry_index);
+		lise_event_entry* entry = blib_darray_get(&registered_events, entry_index);
 
 		if (entry->event_code == event_code)
 		{
 			for (int listener = 0; listener < entry->listeners.size; listener++)
 			{
-				lise_on_event_cb* event_cb = lise_darray_get(&entry->listeners, listener);
+				lise_on_event_cb* event_cb = blib_darray_get(&entry->listeners, listener);
 
 				(*event_cb)(event_code, ctx);
 			}
@@ -74,11 +74,11 @@ void lise_event_add_listener(uint16_t event_code, lise_on_event_cb listener)
 {
 	for (int entry_index = 0; entry_index < registered_events.size; entry_index++)
 	{
-		lise_event_entry* entry = lise_darray_get(&registered_events, entry_index);
+		lise_event_entry* entry = blib_darray_get(&registered_events, entry_index);
 
 		if (entry->event_code == event_code)
 		{
-			lise_darray_push_back(&entry->listeners, &listener);
+			blib_darray_push_back(&entry->listeners, &listener);
 
 			return;
 		}
